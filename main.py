e3f8e3c2338ec995e1195ff89e806baf5aeba62a
Mutavaislam1-boop <mutavaislam1@gmail.com>
2026-06-20 00:33:50 +0500

    add background image

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 @app.get("/")
 async def home():
