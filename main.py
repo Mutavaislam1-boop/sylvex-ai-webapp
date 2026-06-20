1b861b55a5db2891a8353c3ed9f37ae44d7626b0
Mutavaislam1-boop <mutavaislam1@gmail.com>
2026-06-20 10:38:49 +0500

    add save settings endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -6,6 +6,19 @@ app = FastAPI()
 
 app.mount("/image", StaticFiles(directory="image"), name="image")
 
+
 @app.get("/")
 async def home():
     return FileResponse("index.html")
+
+
+@app.post("/save-settings")
+async def save_settings(request: Request):
+    data = await request.json()
+
+    print("SETTINGS:", data)
+
+    return {
+        "success": True,
+        "message": "Settings saved"
+    }
