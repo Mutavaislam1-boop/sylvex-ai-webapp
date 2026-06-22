997470b8f6c91582e48bd1aeadaa3c86a3f06efc
Mutavaislam1-boop <mutavaislam1@gmail.com>
2026-06-22 22:49:23 +0500

    fix cabinet route

--- main.py
+++ main.py
@@ -122,35 +122,7 @@ async def save_settings(request: Request):
 
     text = design(title, body)
 
-    ##if BOT_TOKEN and telegram_id and message_id:
-        response = requests.post(
-            f"https://api.telegram.org/bot{BOT_TOKEN}/editMessageText",
-            json={
-                "chat_id": telegram_id,
-                "message_id": message_id,
-                "text": text,
-                "parse_mode": "HTML",
-                "disable_web_page_preview": False,
-               "reply_markup": {
-                    "inline_keyboard": [
-                        [
-                            {
-                                "text": "⚙️ Настройки модели",
-                                "web_app": {
-                                    "url": f"{WEBAPP_URL}?message_id={message_id}&model=kling"
-                                }
-                            }
-                        ]
-                    ]
-                }
-            },
-            timeout=10
-        )
-
-        print("TELEGRAM STATUS:", response.status_code)
-        print("TELEGRAM RESPONSE:", response.text)
-
-    ##return {
+    return {
         "success": True,
         "message": "✅ Настройки Kling сохранены"
     }
